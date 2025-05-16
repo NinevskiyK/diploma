bb9e7111e9175849995aa2ba5d2aa44e137e360b
nkamil <ninevskykn@gmail.com>
2025-05-16 16:47:05 +0000

    add system optimization experience artifacts

---
+++ exps/system_optimization/optimization/run_optim.py
@@ -0,0 +1,11 @@
+
+from load_testing_sim.optimization.optimize import optimize_params, optimize_time
+import json
+import sys
+
+if __name__ == '__main__':
+    settings_file = sys.argv[1]
+    dir_name = sys.argv[2]
+    with open(settings_file, 'r') as f:
+        settings = json.load(f)
+    optimize_params(settings, dir_name)
